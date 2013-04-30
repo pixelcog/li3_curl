e61c2353209ec32ad7e5ae2dd60ac84ec562e8c9
Mike Greiling <mike@pixelcog.com>
2013-04-29 20:26:48 -0500

    initial commit

---
+++ CurlStackException.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Enhanced Curl Socket for Lithium
+ *
+ * @copyright     Copyright 2013, PixelCog Inc. (http://pixelcog.com)
+ * @license       http://opensource.org/licenses/bsd-license.php The BSD License
+ */
+
+namespace li3_curl\net\socket;
+
+/**
+ * The `CurlStackException` is thrown when an error is encountered in a `curl_multi_***` operation.
+ *
+ * @see li3_multi_curl\net\socket\Curl
+ */
+class CurlStackException extends \Exception {}
+
+?>
